ca697d16ab5366e42c54bc918afc6fcaaac84e81
Asstoria <66348788+clo-cry@users.noreply.github.com>
2023-05-30 21:52:48 +0800

    Update derta.java
    测试修改

--- Test/src/beta/test/D/derta.java
+++ Test/src/beta/test/D/derta.java
@@ -3,5 +3,6 @@ package beta.test.D;
 public class derta {
     public static void main(String[] args) {
         System.out.println("Hello Kitty");
+        System.out.println("Hello Dogge");
     }
 }
