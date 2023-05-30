a885189512d611bbd1c64a6e2e1baae019976656
clo-cry <2364082093@qq.com>
2023-05-30 08:34:06 +0800

    package test

---
+++ Test/src/beta/BetaTest.java
@@ -0,0 +1,8 @@
+package beta;
+
+public class BetaTest {
+    public static void main(String[] args) {
+        System.out.println("The quick brown fox jumps over a lazy dog.");
+        System.out.println("The quick brown fox jumps over a lazy dog.");
+    }
+}
